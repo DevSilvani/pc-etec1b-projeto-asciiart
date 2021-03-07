1e79739e406d3c8a2a609aee3c1dfcca82d832f9
SILVANI CUSTODIO DE OLIVEIRA <silvani.oliveira@etec.sp.gov.br>
2021-03-07 13:36:22 -0300

    C# Pronto

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace pc_etec1b_projeto_asciiart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@" ||  || ");
+            Console.WriteLine(@" \\()// ");
+            Console.WriteLine(@"//(__)\\");
+            Console.WriteLine(@"||    ||");
+            Console.ReadKey();
+        }
+    }
+}
